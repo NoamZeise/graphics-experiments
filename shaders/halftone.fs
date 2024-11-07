ec46bb27fa1b72878d8179c08204a91de56f776e
Noam Zeise <noam4134@gmail.com>
2024-11-07 22:11:47 +0000

    fixed overexposed halftones

--- shaders/halftone.fs
+++ shaders/halftone.fs
@@ -39,23 +39,24 @@ void main() {
   
   float sum_pos = fpos.x+fpos.y+fpos.z;
   float d_x = dFdx(sum_pos) + dFdy(sum_pos);
-		  
-  float size = 0.5*((lambertian-0.7)/-1.4);
 
-  float freq = 20;
+  float freq = 20; // higher -> smaller dots + dot spacing
+  
   grid = vec2(fract(grid.x*freq), fract(grid.y*freq));
   // lines instead of dots
   //grid.y = 0.1;
 
   float d = length(grid - vec2(0.5, 0.5));
-  float range = 0.05 * (abs(d_x)*500);
-  float darkest = 0.3;
+  float range = 0.15 * (abs(d_x)*700);
+  float darkest = 0.8; // 1 -> dots go to black
+  
+  float size = 0.5*((lambertian-0.7)/-1.5); // larger spots when darker
   
   float tone = smoothstep(size - range, size + range, d);
-
-  float tl = lambertian;
-  lambertian = smoothstep(0.2, darkest, lambertian);
-  lambertian = clamp(lambertian, tone + 0.8*abs(tl+0.9), 1.0);
+  tone = clamp(tone, 1 - abs(clamp(lambertian, -darkest, 0.0)), 1.0);
+  
+  lambertian = smoothstep(0.2, 0.5, lambertian);
+  lambertian = clamp(lambertian, tone, 1.0);
 
   colour = c_obj * c_light * lambertian;
   //colour = vec4(vec3(lambertian), 1);

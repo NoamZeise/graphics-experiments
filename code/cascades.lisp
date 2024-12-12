9f738d223dec90fce8031087d1e403aae3d24262
Noam Zeise <noam4134@gmail.com>
2024-12-12 20:10:15 +0000

    using gficl ssbos

--- code/cascades.lisp
+++ code/cascades.lisp
@@ -3,26 +3,24 @@
 (defclass test-compute-post-scene (post-scene) ())
 
 (defclass test-compute-shader (post-shader)
-  ((ssbo :initform 0 :type integer)))
+  ((ssbo :type gficl:storage-buffer)
+   (init :initform nil)))
 
 (defmethod reload ((s test-compute-shader))
   (compute-shader-reload-files (s #p"test-compute.cs") shader))
 
 (defmethod resize ((shader test-compute-shader) (w integer) (h integer))
-  (with-slots (ssbo) shader
-    (if (not (= ssbo 0)) (gl:delete-buffers (list ssbo)))
-    (setf ssbo (gl:gen-buffer))
-    (gl:bind-buffer :shader-storage-buffer ssbo)
-    (cffi:with-foreign-array
-     (arr (make-array (* w h)
-		      :element-type '(float)
-		      :initial-contents
-		      (loop for x from w downto 1 nconcing
-			    (loop for y from 1 to h collecting
-				  (+ (/ x (float w)) (/ y (float h))))))
-	  `(:array :float ,(* w h)))		       
-     (%gl:buffer-data :shader-storage-buffer (* w h (cffi:foreign-type-size :float))
-		      arr :static-read))))
+  (with-slots (ssbo init) shader
+    (if init (gficl:delete-gl ssbo)
+      (setf init t))
+    (setf ssbo
+	  (gficl:make-storage-buffer-from-array (* w h) :float :static-read
+	    (make-array (* w h)
+			:element-type '(float)
+			:initial-contents
+			(loop for x from w downto 1 nconcing
+			      (loop for y from 1 to h collecting
+				    (+ (/ x (float w)) (/ y (float h))))))))))
 
 (defmethod draw ((shader test-compute-shader) (scene test-compute-post-scene))
   (with-slots ((w width) (h height)) scene
@@ -30,9 +28,13 @@
       (gl:active-texture :texture0)
       (gl:bind-texture :texture-2d compute-target)
       (gl:bind-image-texture 0 compute-target 0 nil 0 :read-write :rgba32f)
-      (%gl:bind-buffer-base :shader-storage-buffer 2 (slot-value shader 'ssbo))
+      (gficl:bind-storage-buffer (slot-value shader 'ssbo) 2)      
       (%gl:dispatch-compute w h 1)
       (%gl:memory-barrier '(:shader-image-access-barrier)))))
+
+(defmethod free ((shader test-compute-shader))
+  (gficl:delete-gl (slot-value shader 'ssbo))
+  (call-next-method))
 
 (defclass test-compute-pass (post-pass) ())
 
